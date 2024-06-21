e36c97d8149ed5a28d990d50e348ca57917e42c4
sabberrahman <sabbersr1@gmail.com>
2024-06-21 18:00:57 +0600

    kinde auth , guestbook signin

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -2,7 +2,10 @@ import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { GhostIcon, Github } from "lucide-react";
+import {RegisterLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Form } from "../components/Form";
+
 
 export default function GuestBook(){
     return (
@@ -20,25 +23,31 @@ export default function GuestBook(){
     )
 }
 
-function GuestForm(){
-    const user=false;
+async function GuestForm(){
+   const {getUser}=getKindeServerSession()
+   const user = await getUser()
 
     if (user) {
         return (
-            <h1>hello....</h1>
+            <Form/>
         )
     }
 
     return (
         <div className="flex flex-col justify-between gap-4 md:flex-row mt-2">
             <Input type="text" placeholder="Write Your Message..." className="outline-none"/>
-            <Button> 
+
+            <RegisterLink>
+
+                <Button> 
           
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 mx-1">
   <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
 </svg>
 Sign in
             </Button>
+            </RegisterLink>
+            
         </div>
     )
 }
